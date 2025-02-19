20b3cce078f6725fb3968e0b3aadd438c94126a6
thomas-lanning <102396592+thomas-lanning@users.noreply.github.com>
2025-02-19 13:30:12 +1100

    First Set-up

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/cv')
+def cv():
+    return render_template('cv.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
